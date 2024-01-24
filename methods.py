47b7e63735015e67855d75fdb9ecf10f13598929
Paul Yan <paul.yan@netint.ca>
2024-01-24 12:58:12 -0800

    added improved newton's method

--- methods.py
+++ methods.py
@@ -21,4 +21,27 @@ def newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
         t += step_sz
     return np.array(y_values), np.array(t_vals)
 
+def improved_newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
+    """
+    :param y_0: Initial value of y
+    :param t_0: Initial value of t
+    :param step_sz: Step size
+    :param max_t: Maximum value of t
+    :param diff_eq: Differential equation
+    :return: numpy array of y values at each steps
+    """
+    y = y_0
+    t = t_0
+    y_values = [y_0]
+    t_vals = [t_0]
+    while t < max_t:
+        cur_slope = diff_eq(y, t)
+        y_peak = y + step_sz * cur_slope
+        peak_slope = diff_eq(y_peak, t + step_sz)
+        y = y + step_sz * (cur_slope + peak_slope) / 2
+        y_values.append(y)
+        t_vals.append(t)
+        t += step_sz
+    return np.array(y_values), np.array(t_vals)
+
 
--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import numpy as np
-from methods import newtons_method
+from methods import newtons_method, improved_newtons_method
 import matplotlib.pyplot as plt
 
 def y_prime(y, t):
-    return np.sqrt(y*t)
+    return y
 
 y_0 = 1
 t_0 = 0
-step_sz = 0.01
-max_t = 500
-y_vals, t_vals = newtons_method(y_0, t_0, step_sz, max_t, y_prime)
-plt.plot(t_vals, y_vals)
+step_sz = 0.1
+max_t = 10
+y_vals_newton, t_vals_newton = newtons_method(y_0, t_0, step_sz, max_t, y_prime)
+y_vals_improved_newton, t_vals_improved_newton = improved_newtons_method(y_0, t_0, step_sz, max_t, y_prime)
+plt.plot(t_vals_newton, y_vals_newton, label="Newton's Method")
+plt.plot(t_vals_improved_newton, y_vals_improved_newton, label="Improved Newton's Method")
+plt.legend()
 plt.show()
