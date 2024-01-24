0f660b53a232e019f00f9447339eee3699bdfb13
Paul Yan <paul.yan@netint.ca>
2024-01-24 13:36:51 -0800

    added werid euler

--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
+def eulers_method(y_0, t_0, step_sz, max_t, diff_eq):
     """
     :param y_0: Initial value of y
     :param t_0: Initial value of t
@@ -21,7 +21,7 @@ def newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
         t += step_sz
     return np.array(y_values), np.array(t_vals)
 
-def improved_newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
+def improved_eulers_method(y_0, t_0, step_sz, max_t, diff_eq):
     """
     :param y_0: Initial value of y
     :param t_0: Initial value of t
@@ -44,4 +44,24 @@ def improved_newtons_method(y_0, t_0, step_sz, max_t, diff_eq):
         t += step_sz
     return np.array(y_values), np.array(t_vals)
 
+def weird_euler(y_0, t_0, step_sz, max_t, diff_eq):
+    """
+    :param y_0: Initial value of y
+    :param t_0: Initial value of t
+    :param step_sz: Step size
+    :param max_t: Maximum value of t
+    :param diff_eq: Differential equation
+    :return: numpy array of y values at each steps
+    """
+    y = y_0
+    t = t_0
+    y_values = [y_0]
+    t_vals = [t_0]
+    while t < max_t:
+        y_peak = y + step_sz * diff_eq(y, t)
+        y = y + step_sz * diff_eq(y_peak, t + step_sz)
+        y_values.append(y)
+        t_vals.append(t)
+        t += step_sz
+    return np.array(y_values), np.array(t_vals)
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from methods import newtons_method, improved_newtons_method
+from methods import eulers_method, improved_eulers_method, weird_euler
 import matplotlib.pyplot as plt
 
 def y_prime(y, t):
@@ -9,9 +9,12 @@ y_0 = 1
 t_0 = 0
 step_sz = 0.1
 max_t = 10
-y_vals_newton, t_vals_newton = newtons_method(y_0, t_0, step_sz, max_t, y_prime)
-y_vals_improved_newton, t_vals_improved_newton = improved_newtons_method(y_0, t_0, step_sz, max_t, y_prime)
-plt.plot(t_vals_newton, y_vals_newton, label="Newton's Method")
-plt.plot(t_vals_improved_newton, y_vals_improved_newton, label="Improved Newton's Method")
+y_vals_newton, t_vals_newton = eulers_method(y_0, t_0, step_sz, max_t, y_prime)
+y_vals_improved_newton, t_vals_improved_newton = improved_eulers_method(y_0, t_0, step_sz, max_t, y_prime)
+y_vals_weird_euler, t_vals_weird_euler = weird_euler(y_0, t_0, step_sz, max_t, y_prime)
+plt.plot(t_vals_newton, y_vals_newton, label="Euler's Method")
+plt.plot(t_vals_improved_newton, y_vals_improved_newton, label="Improved Euler's Method")
+plt.plot(t_vals_weird_euler, y_vals_weird_euler, label="Weird Euler's Method")
+plt.plot(t_vals_newton, np.exp(t_vals_newton), label="Actual")
 plt.legend()
 plt.show()
